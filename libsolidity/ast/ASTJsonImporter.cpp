4ac7cdc6ce6bf6ddde07b7330d0522794893991c
Matheus Aguiar <matheus.pit@gmail.com>
2024-04-06 17:17:51 -0300

    Make parser accept transient as location (but not a keyword)

--- libsolidity/ast/ASTJsonImporter.cpp
+++ libsolidity/ast/ASTJsonImporter.cpp
@@ -1188,6 +1188,8 @@ VariableDeclaration::Location ASTJsonImporter::location(Json::Value const& _node
 		return VariableDeclaration::Location::Memory;
 	else if (storageLocStr == "calldata")
 		return VariableDeclaration::Location::CallData;
+	else if (storageLocStr == "transient")
+		return VariableDeclaration::Location::Transient;
 	else
 		astAssert(false, "Unknown location declaration");
 
