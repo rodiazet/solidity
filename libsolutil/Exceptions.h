cbe8b48860ebbb487451531bd59b0fb98f556e05
r0qs <rodrigo.saramago@ethereum.org>
2024-04-23 22:00:43 +0200

    Replace custom exception by asserts

--- libsolutil/Exceptions.h
+++ libsolutil/Exceptions.h
@@ -76,7 +76,6 @@ DEV_SIMPLE_EXCEPTION(FileNotFound);
 DEV_SIMPLE_EXCEPTION(NotAFile);
 DEV_SIMPLE_EXCEPTION(DataTooLong);
 DEV_SIMPLE_EXCEPTION(StringTooLong);
-DEV_SIMPLE_EXCEPTION(ElementNotFound);
 
 // error information to be added to exceptions
 using errinfo_comment = boost::error_info<struct tag_comment, std::string>;
